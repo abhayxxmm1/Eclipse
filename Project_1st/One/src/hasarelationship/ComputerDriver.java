f581dcda345952f79caddc10934ebca7defd5abb
Abhay <ghongadeabhay60@gmail.com>
2025-07-03 23:02:16 +0530

    also has a null pointer exeption

--- Project_1st/One/src/hasarelationship/ComputerDriver.java
+++ Project_1st/One/src/hasarelationship/ComputerDriver.java
@@ -5,9 +5,12 @@ public class ComputerDriver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-//		Computer c1 = new Computer("HP","I-5",8,500,4000.0);
-		Computer c1 = null;
+		Computer c1 = new Computer("HP","I-5",8,500,4000.0);
+//		Computer c1 = null; 	// NullPointerExeption
 		c1.displayComputer();
+		c1.insertPendrive("Sandisk", 32, 40, 5000);
+		
+		c1.p.displayPendrive();
 	}
 
 }
