eedf3b03011ff3c0b5136eea84572a65e42ca402
Abhay <ghongadeabhay60@gmail.com>
2025-07-29 22:30:31 +0530

    throws

--- Project_1st/One/src/exception/Exp6.java
+++ Project_1st/One/src/exception/Exp6.java
@@ -1,13 +1,20 @@
 package exception;
 
 public class Exp6 {
+	/**
+	 * You only need to include a throws clause on a method if the method throws 
+	 * a checked exception example:- I/OExecption. 
+	 * If the method throws a runtime exception then there is no need to do so.
+	 * @param args
+	 * @throws InterruptedException
+	 */
 
 	public static void main(String[] args) throws InterruptedException{
 		for(int i=0; i<10; i++ )
 		{
 			System.out.println("sir is intelligent");
 			
-			Thread.sleep(500);
+			Thread.sleep(500);	// https://www.google.com/search?client=safari&rls=en&q=work+of+thread.sleep+in+java&ie=UTF-8&oe=UTF-8
 		}
 	}
 }
