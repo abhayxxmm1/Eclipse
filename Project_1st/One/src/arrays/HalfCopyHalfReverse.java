c1478e1454b374d3448c994bdd43856f01c558e6
Abhay <ghongadeabhay60@gmail.com>
2025-08-05 18:12:45 +0530

    Merging arrays: half copy half merge

---
+++ Project_1st/One/src/arrays/HalfCopyHalfReverse.java
@@ -0,0 +1,30 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class HalfCopyHalfReverse {
+	public static void main(String[] args) {
+		int[]a = {10,20,30,40,50,60};
+		int[]b = {90,80,70,60,50};
+		int[]c = new int[a.length+b.length] ;
+		
+		for (int i=0; i<c.length; i++)
+		{
+			if(i<a.length)
+			{
+				c[i] = a[i];
+			}
+			else
+			{
+				c[a.length+a.length+b.length-1-i] = b[i-a.length];
+			}
+		}
+		System.out.println(c);
+		for (int i=0; i<c.length; i++)
+		{
+			System.out.println(c[i]);
+		}
+		System.out.println(c.length);
+		System.out.println(Arrays.toString(c));
+	}
+}
