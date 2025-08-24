13c93ac9373f498ef2ff66e1504b90b98ee0decc
Abhay <ghongadeabhay60@gmail.com>
2025-08-25 02:35:06 +0530

    multiplication of 2d array

---
+++ Project_1st/One/src/arrays/TwoDimMulti.java
@@ -0,0 +1,52 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class TwoDimMulti {
+
+	public static void main(String[] args) {
+		
+		int [][]a = {
+				{1,2,3},
+				{4,5,6},
+				{7,8,9}
+		};
+		int [][]b = {
+				{10,20,30},
+				{40,50,60},
+				{70,80,90}
+		};
+		int [][]c= new int[3][3];
+		
+		multi(a, b, c);
+		for (int i = 0; i < c.length; i++) {
+		    for (int j = 0; j < c[0].length; j++) {
+		        System.out.print(c[i][j] + " ");
+		    }
+		    System.out.println();
+		}
+		
+
+		
+		
+	}
+	
+	public static int[][] multi(int[][]a,int[][]b,int[][]c)
+	{
+		for (int i = 0; i < a.length; i++) {
+			
+			for (int j = 0; j < a[i].length; j++) {
+				
+				int sum = 0;
+				
+				for (int k = 0; k < a[j].length; k++) {
+					
+					sum=sum+a[i][k]*b[k][j];
+				}
+				c[i][j] = sum;
+			}
+		}
+		return c; 
+	}
+}
