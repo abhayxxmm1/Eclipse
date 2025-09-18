1f87f6b44d3622dbd78ea8cddd98011a20804f09
Abhay <ghongadeabhay60@gmail.com>
2025-09-18 23:43:15 +0530

    Char array to String

---
+++ Project_1st/One/src/Strings/S6.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class S6 {
+
+	public static void main(String[] args) {
+		
+		char [] ar = {'s','u' ,'c' , 'c'};
+		String string  = new String(ar);
+		
+		System.out.println(string);
+		
+	}
+}
